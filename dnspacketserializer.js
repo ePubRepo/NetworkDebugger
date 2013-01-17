70faae8500417d65aa24b1ab14bb4ead5d83dfcf
no name <your@gmail.com>
2013-01-17 11:47:43 -0500

    major breaking with all data writing linted and cleaned up

--- dnspacketserializer.js
+++ dnspacketserializer.js
@@ -19,27 +19,60 @@ DNSPacketSerializer.prototype.dnsPacket_ = null;
  * @return {ArrayBuffer} Serialized DNS packet.
  */
 DNSPacketSerializer.prototype.serialize = function() {
-    var dnsSerializer = new Serializer();
+    var dataSerializer = new Serializer();
     var arrPacketSections = [DNSUtil.PacketSection.QUESTION,
              DNSUtil.PacketSection.ANSWER,
              DNSUtil.PacketSection.AUTHORITY,
              DNSUtil.PacketSection.ADDITIONAL];
 
-    dnsSerializer.short(0).short(this.dnsPacket_.flags_);
+    dataSerializer.short(0).short(this.dnsPacket_.flags_);
 
     arrPacketSections.forEach(function(packetSection) {
-        dnsSerializer.short(this.dnsPacket_.data_[packetSection].length);
+        dataSerializer.short(this.dnsPacket_.data_[packetSection].length);
     }.bind(this));
 
     arrPacketSections.forEach(function(packetSection) {
       this.dnsPacket_.data_[packetSection].forEach(function(dnsRecord) {
-          dnsSerializer.name(dnsRecord.name_).
+          this.serializeName(dnsRecord.name_, dataSerializer).
              short(dnsRecord.type_).short(dnsRecord.cl_);
-      });
+      }.bind(this));
     }.bind(this));
 
-    return dnsSerializer.getBuffer();
+    return dataSerializer.getBuffer();
 };
+
+/**
+ * Writes a DNS name to a specified data serializer.
+ * If opt_ref is specified, will finish this name with a
+ * suffix reference (i.e., 0xc0 <ref>). If not, then will terminate with a NULL
+ * byte.
+ *
+ * @param {string} dnsName A DNS name such as "mail.google.com".
+ * @param {Serializer} dnsSerializer Data serializer being used to serialize
+ *                                   a DNS packet.
+ * @param {integer} opt_ref Packet location of DNS name being referenced.
+ *                            See Section 4.1.4 of RFC 1035.
+ * @return {Serializer} This instance of a Serializer.
+ */
+DNSPacketSerializer.prototype.serializeName = function(dnsName,
+                                                       dnsSerializer,
+                                                       opt_ref) {
+    var parts = dnsName.split('.');
+    parts.forEach(function(part) {
+        dnsSerializer.byte(part.length);
+        for (var i = 0; i < part.length; ++i) {
+            dnsSerializer.byte(part.charCodeAt(i));
+        }
+    }.bind(this));
+
+    if (opt_ref) {
+        dnsSerializer.byte(0xc0).byte(opt_ref);
+    } else {
+        dnsSerializer.byte(0);
+    }
+    return dnsSerializer;
+};
+
 
 /**
  * Serializer writes an object data to an ArrayBuffer.
@@ -86,33 +119,8 @@ Serializer.prototype.short = function(b) {
 };
 
 /**
- * Writes a DNS name. If opt_ref is specified, will finish this name with a
- * suffix reference (i.e., 0xc0 <ref>). If not, then will terminate with a NULL
- * byte.
- * 
- * @param {string} dnsName A DNS name such as "mail.google.com"
- * @return {Serializer} This instance of a Serializer.
- */
-Serializer.prototype.name = function(dnsName, opt_ref) {
-    var parts = dnsName.split('.');
-    parts.forEach(function(part) {
-        this.byte(part.length);
-        for (var i = 0; i < part.length; ++i) {
-            this.byte(part.charCodeAt(i));
-        }
-    }.bind(this));
-  
-    if (opt_ref) {
-        this.byte(0xc0).byte(opt_ref);
-    } else {
-        this.byte(0);
-    }
-    return this;
-};
-
-/**
  * Return serialized ArrayBuffer representation of an object.
- * @return {ArrayBuffer}
+ * @return {ArrayBuffer} ArrayBuffer representation of an object.
  */
 Serializer.prototype.getBuffer = function() {
     return this.buffer_;
